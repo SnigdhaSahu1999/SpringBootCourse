a6144684a9610c483da4feb612052f9414907a5f
Snigdha <snigdha.sahu1999@gmail.com>
2023-04-09 23:33:38 +0530

    CricketCoach.java added

---
+++ 02-spring-boot-spring-core/01-constructor-injection/springcoredemo/src/main/java/com/snigdha/springcoredemo/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.snigdha.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+@Component //marks the class as a Spring bean and makes it available for Dependency Injection
+public class CricketCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout(){
+        return "Practice fast bowling for 15 minutes!";
+    }
+}
