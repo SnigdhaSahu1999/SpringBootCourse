47d914be5e86c49bc82ca5f975572be5ff3b185e
Snigdha <snigdha.sahu1999@gmail.com>
2023-04-10 22:36:37 +0530

    04-bean-lifecycle-methods added

---
+++ 02-spring-boot-spring-core/04-bean-lifecycle-methods/springcoredemo/src/main/java/com/snigdha/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,28 @@
+package com.snigdha.springcoredemo.common;
+
+import com.snigdha.springcoredemo.common.Coach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component //marks the class as a Spring bean and makes it available for Dependency Injection
+public class CricketCoach implements Coach {
+
+    //definr ur init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff(): "+ getClass().getSimpleName());
+    }
+
+    //define ur destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff(): "+ getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout(){
+        return "Practice fast bowling for 15 minutes!";
+    }
+}
