aeb486f5e31bcdce91fc42ae5a65ceb13f5c8241
Snigdha <snigdha.sahu1999@gmail.com>
2023-05-01 00:11:42 +0530

    EmployeeDAO.java added

---
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/snigdha/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.snigdha.springboot.cruddemo.dao;
+
+import com.snigdha.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
