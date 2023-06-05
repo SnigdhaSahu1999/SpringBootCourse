a48a2a080da2a84cb492d9f25dec25535ba92206
Snigdha <snigdha.sahu1999@gmail.com>
2023-06-06 00:03:50 +0530

    DemoController.java added

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/snigdha/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.snigdha.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class DemoController {
+
+    // Create a mapping for "/hello
+
+    @GetMapping("/hello")
+    public String sayHello(Model theModel){
+
+        theModel.addAttribute("theDate",new java.util.Date());
+
+        return "helloworld";
+        //Since we have thymeleaf dependency in MAVEN POM, SB will autoconfigure to use Thymeleaf
+
+
+    }
+}
