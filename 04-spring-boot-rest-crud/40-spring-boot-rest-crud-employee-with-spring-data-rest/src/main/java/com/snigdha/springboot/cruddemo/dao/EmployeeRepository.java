89b2ec3b33291bcbd1c0e99e35c1ba0b606af957
Snigdha <snigdha.sahu1999@gmail.com>
2023-09-17 11:01:36 +0530

    EmployeeRepository.java updated

--- 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/snigdha/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/snigdha/springboot/cruddemo/dao/EmployeeRepository.java
@@ -4,8 +4,11 @@ import com.snigdha.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-/* @RepositoryRestResource(path="members")
-now the URL will be http://localhost:8080/magic-api/members  */
+/* @RepositoryRestResource(path="members")*/
+
+/*for this app the URL will be http://localhost:8090/magic-api/employees  */
+
+//<Employee, Integer> : Employee is the entity name and primary key of Employee is having datatype int
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     //Entity type - Employee, Primary key - Integer
