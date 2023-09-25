984b005c463f0e96d870cbd924ee99b3254f2bdb
Snigdha <snigdha.sahu1999@gmail.com>
2023-09-25 21:14:53 +0530

    MembershipDAOImpl.java updated

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/snigdha/aopdemo/dao/MembershipDAOImpl.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/snigdha/aopdemo/dao/MembershipDAOImpl.java
@@ -3,11 +3,18 @@ package com.snigdha.aopdemo.dao;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class AccountDAOImpl implements AccountDAO{
+public class MembershipDAOImpl implements MembershipDAO{
 
     @Override
-    public void addAccount() {
+    public boolean addSillyMembers() {
 
-        System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+        System.out.println(getClass() + ": DOING MY DB WORK: ADDING A MEMBERSHIP ACCOUNT");
+        return true;
+    }
+
+    @Override
+    public void goToSleep() {
+
+        System.out.println(getClass() + ": I'm going to sleep now...");
     }
 }
